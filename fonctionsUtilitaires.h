4fcaf281b0f28a779dc9a5366538982d8e296256
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2016-09-22 23:31:03 -0400

    declaration du prototype de la fonction afficherTableau (qui n'etait qu'implementee sans etre definie...)

--- fonctionsUtilitaires.h
+++ fonctionsUtilitaires.h
@@ -15,5 +15,6 @@ void triBulle(int tabDonnees[MAX_VALEURS]);
 void tri2d(int tabDonnees[][MAX_VALEURS], int);
 int occurencesPlusGrand(int tabDonnees[][MAX_VALEURS],int, int);
 bool existe(int tabDonnees[][MAX_VALEURS],int, int p_valeur);
+void afficherTableau(int p_tabDonnees[][MAX_VALEURS], int p_nombreLignes);
 
 #endif /* FONCTIONSUTILITAIRES_H_ */
